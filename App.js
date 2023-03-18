a49f6648f3b01a3a1dbde44686cb481753b144b8
altrealm <126050488+altrealm@users.noreply.github.com>
2023-03-18 16:45:30 +0700

    testing

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Header from './Header';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import TinderCards from './TinderCards';
+import TinderCards2 from "./TinderCards2";
+import SwipeButtons from "./SwipeButtons";
+import Chats from "./Chats";
+import ChatScreen from "./ChatScreen";
+import Map from "./Map";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <Header />
+              <TinderCards />
+              {/*<SwipeButtons />*/}
+            </div>} 
+          />
+          <Route path="/chat" element={
+            <div>
+              <Header backButton="/" />
+              <Chats />
+            </div>} 
+          />
+          <Route path="/chat/:person" element={
+            <div>
+              <Header backButton="/chat" />
+              <ChatScreen />
+            </div>} 
+          />
+          <Route path="/map" element={
+            <div>
+              <Header backButton="/" />
+              <Map />
+            </div>} 
+          />
+        
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
